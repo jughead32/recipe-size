095e7027262e5d21560f8a2f58a8cae40a2d3618
jughead32 <parbaked@gmail.com>
2018-08-12 13:48:24 -0700

    late night changes i dont remember. trying to figure out getElementById

--- recipesize.js
+++ recipesize.js
@@ -1,23 +1,18 @@
 /*This is an app to figure out the total recipe for a given dough size using
- bakers percentages*/
-
+ bakers percentages user will check dough recipe, enter desired recipe weight. recipe will be printed to page for user to print.*/
 
 // array variables containing percentages for doughs [flour,water,salt,yeast]
 let french = [100, 66, 2, 1.2];
 let butterMilk = [];
 
-
-let recipeWeight; // desired recipe weight
-let recipe = alert('Which recipe do you wish to mix?'); //chosen recipe
-let finalRecipe = [] //final array to be printed
-console.log(recipe);
+let recipeWeight = document.getElementById('weight').value;// desired recipe weight
+console.log(recipeWeight)
 
 //factor used to figure out recipe
-
-let conversionFactor = recipeWeight / recipe.reduce((total, amount) => total + amount);
-
+let conversionFactor = recipeWeight / french.reduce((total, amount) => total + amount);
 
 //function creating final recipe array to be printed to page and used
+let newRecipe = [];
 function finalRecipe (arr){
   for (let i = 0; i < arr.length; i++){
     console.log(i);
